3868b78b0f005d42eb041faf4ccf6e10507dc637
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-02-29 17:34:12 -0600

    start OAuth implementation

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+/// is this supposed to be changed to mongoose.connect(process.env.DATABASE_URL, {}) ?? see connecting atlas lesson
+// mongoose.connect('mongodb://localhost/bobateas', {
+//   useNewUrlParser: true, 
+//   useCreateIndex: true, 
+//   useUnifiedTopology: true
+// });
 
-mongoose.connect('mongodb://localhost/bobateas', {
-  useNewUrlParser: true, 
-  useCreateIndex: true, 
-  useUnifiedTopology: true
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require('express-session');
 
 require('dotenv').config();
 
@@ -23,6 +24,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+  secret:'BobaRocks!',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use("/", indexRouter);
 app.use("/bobateas", bobateasRouter);
