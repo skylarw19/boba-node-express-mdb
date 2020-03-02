60617dafb4eabb8bf863a089cdeb98c58b5412fd
Skylar Wong <skylar@Skylars-Air.lan>
2020-03-01 19:26:15 -0600

    OAuth login works

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,8 @@ module.exports = {
 function index(req, res) {
   User.find({}, function(err, users) {
     if (err) return next(err);
-    res.render("users/index", { users });
+    res.render("users/index", { 
+        users,
+        user: req.user }); //step 9
   });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'User Index' });
+});
+
+ // Google OAuth login route
+ router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+ // Google OAuth callback route
+ router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/users',
+    failureRedirect : '/users'
+  }
+));
+
+ // OAuth logout route
+ router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/users');
 });
 
 module.exports = router;
